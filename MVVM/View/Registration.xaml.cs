e0b65feb8d47c52f6e4b11160e6afd4988a508e6
matveevaolga <matveevaolga2882@gmail.com>
2024-01-30 21:56:43 +0300

    Processing user registration
    - (in Registration.xaml) creating Registration window markup
    - (in Registration.cs) creating ResetTextBox function for changing hints in password, email and login TextBoxes;
    realization of INotifyPropertyChanged interface functional for binding code variables to hints text in password, email and login TextBoxes; creating SignUp function for processing pressing registration button

--- MVVM/View/Registration.xaml.cs
+++ MVVM/View/Registration.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using MovieRecommendationsApp.MVVM.Model;
+using MovieRecommendationsApp.MVVM.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,12 +21,41 @@ namespace MovieRecommendationsApp.MVVM.View
     /// <summary>
     /// Логика взаимодействия для Registration.xaml
     /// </summary>
-    public partial class Registration : Window
+    public partial class Registration : Window, INotifyPropertyChanged
     {
+        string loginHint;
+        public string LoginHint
+        {
+            get { return loginHint; }
+            set
+            { loginHint = value; OnPropertyChanged("LoginHint"); }
+        }
+        string passwordHint;
+        public string PasswordHint
+        {
+            get { return passwordHint; }
+            set
+            { passwordHint = value; OnPropertyChanged("PasswordHint"); }
+        }
+        string emailHint;
+        public string EmailHint
+        {
+            get { return emailHint; }
+            set
+            { emailHint = value; OnPropertyChanged("EmailHint"); }
+        }
         public Registration()
         {
             InitializeComponent();
+            LoginHint = "Введите логин";
+            PasswordHint = "Введите пароль";
+            EmailHint = "Введите почту";
+            DataContext = this;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
         private void OpenAuthorizationWindow(object sender, RoutedEventArgs e)
         {
@@ -30,6 +63,43 @@ namespace MovieRecommendationsApp.MVVM.View
             authWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             authWindow.Show();
             this.Close();
+        }
+
+        private void SignUp(object sender, RoutedEventArgs e)
+        {
+            string login = loginTextBox.Text;
+            string password = passwordTextBox.Text;
+            string email = emailTextBox.Text;
+            if (login == "" | password == "")
+            {
+                if (login == "") LoginHint = "Вы не ввели логин";
+                if (password == "") PasswordHint = "Вы не ввели пароль";
+                if (email == "") EmailHint = "Вы не ввели почту";
+                return;
+            }
+            if (DBHelpFunctional.LoginExists(login)) 
+                { LoginHint = "Пользователь с таким логином уже зарегистрирован"; return; }
+            DBHelpFunctional.Register(login, password, email);
+            toAuthorization.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+        }
+
+        private void ResetTextBox(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            switch (textBox.Name)
+            {
+                case "loginTextBox":
+                    LoginHint = "Введите логин";
+                    break;
+                case "passwordTextBox":
+                    PasswordHint = "Введите пароль";
+                    break;
+                case "emailTextBox":
+                    EmailHint = "Введите почту";
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
