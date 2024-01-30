279befb98a5cc8e0f402a5550dfe3f7d0d7936ce
matveevaolga <matveevaolga2882@gmail.com>
2024-01-30 19:49:56 +0300

    Creating functions in ViewModel for processing authorization and registration
    - (id DBHelpFunctional) creating DBHelpFunctional class for connecting user input with DBFunctions functional; creating LoginExists, PasswordCorrect, Register functions for authorization and registration processing
    - (in UserDataProcessing) creating UserDataProcessing class; creating GetPasswordHash for hashing password inputed by user

---
+++ MVVM/ViewModel/DBHelpFunctional.cs
@@ -0,0 +1,37 @@
+﻿using MovieRecommendationsApp.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRecommendationsApp.MVVM.ViewModel
+{
+    public class DBHelpFunctional
+    {
+        public static bool LoginExists(string login)
+        {
+            DBFunctions dBFunctions = new DBFunctions();
+            return dBFunctions.LoginExists(login);
+        }
+
+        public static bool PasswordCorrect(string login, string password)
+        {
+            DBFunctions dBFunctions = new DBFunctions();
+            password = UserDataProcessing.GetPasswordHash(password);
+            bool check = dBFunctions.PasswordCorrect(login, password);
+            return check;
+        }
+
+        public static void Register(string login, string password, string email)
+        {
+            password = UserDataProcessing.GetPasswordHash(password);
+            DBFunctions dBFunctions = new DBFunctions();
+            int profileId = dBFunctions.AddToProfilesTable(login, password, email);
+            if (profileId == 0) { dBFunctions.DeleteProflile(profileId); return; }
+            try { dBFunctions.AddToUsersTable(login, password, email, profileId); }
+            catch (Exception ex) 
+                { Console.WriteLine(ex.GetType().Name); dBFunctions.DeleteProflile(profileId); }
+        }
+    }
+}
---
+++ MVVM/ViewModel/UserDataProcessing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace MovieRecommendationsApp.MVVM.ViewModel
+{
+    public class UserDataProcessing
+    {
+        public static string GetPasswordHash(string password)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
