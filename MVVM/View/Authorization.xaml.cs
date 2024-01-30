153c2d8a6ab3a18542f802dcd00a4a0738b0c74d
matveevaolga <matveevaolga2882@gmail.com>
2024-01-30 21:20:31 +0300

    Processing user authorization
    - (in Authorization.xaml) creating Authorization window markup
    - (in Authorization.cs) creating OpenMainWindow function for opening main window; creating ResetTextBox function for changing hints in password and login TextBoxes;
    realization of INotifyPropertyChanged interface functional for binding code variables to hints text in password and login TextBoxes; creating LogIn function for processing pressing authorization button

--- MVVM/View/Authorization.xaml.cs
+++ MVVM/View/Authorization.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using MovieRecommendationsApp.MVVM.View;
+using MovieRecommendationsApp.MVVM.ViewModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,11 +18,65 @@ namespace MovieRecommendationsApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class Authorization : Window
+    public partial class Authorization : Window, INotifyPropertyChanged
     {
+        string loginHint;
+        public string LoginHint
+        {
+            get { return loginHint; }
+            set
+            { loginHint = value; OnPropertyChanged("LoginHint"); }
+        }
+        string passwordHint;
+        public string PasswordHint
+        {
+            get { return passwordHint; }
+            set
+            { passwordHint = value; OnPropertyChanged("PasswordHint"); }
+        }
         public Authorization()
         {
             InitializeComponent();
+            LoginHint = "Введите логин";
+            PasswordHint = "Введите пароль";
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void LogIn(object sender, RoutedEventArgs e)
+        {
+            string login = loginTextBox.Text;
+            string password = passwordTextBox.Text;
+            if (login == "" | password == "")
+            {
+                if (login == "") LoginHint = "Вы не ввели логин";
+                if (password == "") PasswordHint = "Вы не ввели пароль";
+                return;
+            }
+            if (!DBHelpFunctional.LoginExists(login)) 
+                { LoginHint = "Пользователя под введенным логином не существует"; return; }
+            if (!DBHelpFunctional.PasswordCorrect(login, password)) 
+                { PasswordHint = "Введен неверный пароль"; return; }
+            OpenMainWindow();
+        }
+
+        private void ResetTextBox(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            switch (textBox.Name)
+            {
+                case "loginTextBox":
+                    LoginHint = "Введите логин";
+                    break;
+                case "passwordTextBox":
+                    PasswordHint = "Введите пароль";
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void OpenRegistrationWindow(object sender, RoutedEventArgs e)
@@ -27,6 +84,14 @@ namespace MovieRecommendationsApp
             Registration regWindow = new Registration();
             regWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             regWindow.Show();
+            this.Close();
+        }
+
+        private void OpenMainWindow()
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            mainWindow.Show();
             this.Close();
         }
     }
