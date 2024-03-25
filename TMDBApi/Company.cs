fc0f1dba0b9b8cc410235b22bd4e37e6bc85c629
matveevaolga <matveevaolga2882@gmail.com>
2024-03-26 02:22:04 +0300

    Getting data from api to form a page with full movie info
    - Creating MovieDetails class to represent additional movie info
    - Creating Company class to represent ProductionCompanies property of MovieDetails
    - (in ApiQueriesProcessing) creating GetMovieDetails method for getting additonal movie info using TMDB api
    - (in MovieInfoPreview) creating GoToMoviePage method to redirect user to MoviePage when clicking movieTitle button
    - (in MoviePage) passing to constructor Movie and MovieDetails objects

---
+++ TMDBApi/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TMDBApi
+{
+    public class Company
+    {
+        public long Id { get; init; }
+        public string? Name { get; init; }
+        [JsonPropertyName("origin_country")]
+        public string? OriginCountry { get; init; }
+    }
+}
---
+++ TMDBApi/MovieDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TMDBApi
+{
+    public class MovieDetails
+    {
+        public bool Adult { get; init; }
+        public int Revenue { get; init; }
+        public int Budget { get; init; }
+        public string? Homepage { get; init; }
+        public int Runtime { get; init; }
+        [JsonPropertyName("production_companies")]
+        public List<Company>? ProductionCompanies {get; init;}
+        public string? Status { get; init; }
+        [JsonPropertyName("vote_average")]
+        public double VoteAverage { get; init; }
+        [JsonPropertyName("vote_count")]
+        public int VoteCount { get; init; }
+    }
+}
